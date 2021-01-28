57b1a3fb50e1a7043d3a99ef487a4b108730c8b7
Ahmad Syafrudin <kemanayaa@gmail.com>
2021-01-28 15:00:46 +0700

    Update ShiftSchedule.cs
    remove unused using

--- SHRIS.EF/Models/ShiftSchedule.cs
+++ SHRIS.EF/Models/ShiftSchedule.cs
@@ -1,9 +1,4 @@
 ﻿using SHRIS.EF.Models.Interface;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SHRIS.EF.Models
 {
